a8af4da12127c11608f8241530416b5d772351db
m-maina12 <150496376+m-maina12@users.noreply.github.com>
2026-03-11 10:46:17 +0100

    Update Ridge_fitmodel.R

--- LinearShrinkage/Ridge_fitmodel.R
+++ LinearShrinkage/Ridge_fitmodel.R
@@ -1,35 +1,31 @@
-# Ridge  
+# Ridge 
 library(glmnet)
 library(matrixStats)
 
-Ridge_fit <- function(y, X, t, newX, second_stage, N_sample_bootstrap = 100){
-    # y = list of outcomes
-    # X = list of datasets with covariates
-    # t = list of treatment
+ridge_predict_ite <- function(model, # Model object
+                              x1_test,
+                              x0_test){
+  ite <- as.numeric(predict(model, newx = x1_test, s = "lambda.min") -
+                      predict(model, newx = x0_test, s = "lambda.min"))
 
-    ols_iv_variance <- function(data_train, newX, model_formula){
-      get_linear_model_variance(
-          data_train = data_train,
-          new_patients_data = newX,
-          formula = model_formula
-      )
-    }
+  return(ite)
+}
 
-    bootstrap_variance <- function(data_train, newX, model_formula,
-                               x1_test, x0_test,
-                               penalty_factors, lambda_seq,
-                               N_boot = 100){
+ridge_bootstrap_variance <- function(data_train, # Original training data
+                                     newX, # New covariates
+                                     model_formula, # Model formula
+                                     penalty_factors, # Penalty factors for the regression 
+                                     lambda_seq, # Grid of lambda parameters
+                                     N_boot = 100 # Numbers of bootstrap iteration
+                                     ){
 
   bootstrap_preds <- matrix(nrow = nrow(newX), ncol = N_boot)
 
   for(m in 1:N_boot){
-
       bs_data <- get_bootstrap_sample(X = data_train)
-
-      x_train_bs <- model.matrix(model_formula, data = bs_data)[, -1]
-
+      x_train_bs <- ridge_build_design_matrix(X = bs_data, mod.formula = model_formula)
       cv_bs <- cv.glmnet(
-          x_train_bs,
+          x = x_train_bs,
           y = bs_data$y,
           nfolds = 5,
           alpha = 0,
@@ -37,58 +33,58 @@ Ridge_fit <- function(y, X, t, newX, second_stage, N_sample_bootstrap = 100){
           lambda = lambda_seq
       )
 
-      bootstrap_preds[, m] <- as.numeric(
-          predict(cv_bs, newx = x1_test, s = "lambda.min") -
-          predict(cv_bs, newx = x0_test, s = "lambda.min")
-      )
+      bootstrap_preds[, m] <- ridge_predict_ite(model = cv_bs, newX = newX)
   }
 
   matrixStats::rowVars(bootstrap_preds)
 }
 
-    n_covariates <- ncol(X[[1]])
-    nstudies <- length(y)
+ridge_build_design_matrix <- function(X, mod.formula){
+  return(model.matrix(mod.formula, data = X)[, -1])
+}
+
+ridge_ipd_ite <- function(data, 
+                          newX, 
+                          covariate_names, 
+                          outcome = "response",
+                          treatment = "trt",  
+                          second_stage, 
+                          N_sample_bootstrap = 100
+                          ){
+    n_covariates <- length(covariate_names)
+    nstudies <- length(data)
 
     lambda_seq <- 10 ^ seq(2, -3, by = -0.3)
     penalty_factors <- c(rep(0, 1 + n_covariates), rep(1, n_covariates))
 
-    new_cov_names <- paste0("x", 1:n.covariates)
-    formula_str <- paste("y ~ treatment * (", paste(new_cov_names, collapse = " + "), ")", 
+    formula_str <- paste("y ~ ", treatment, " * (", paste(covariate_names, collapse = " + "), ")", 
                          sep = "")
     model_formula <- as.formula(formula_str)
 
-    x1_test <- cbind(1, newX, newX) 
-    x0_test <- cbind(0, newX, 0 * newX) 
     predictions <- variance <- matrix(nrow = nrow(newX), ncol = nstudies)
+    x1_test <- model.matrix(model_formula, data = cbind(newX, treatment = 1))[,-1]
+    x0_test <- model.matrix(model_formula, data = cbind(newX, treatment = 0))[,-1] 
     
-    second_stage <- toupper(second_stage)
-    if(second_stage == "OLS+IV"){
-      variance_method <- ols_iv_variance
-    } else (second_stage == "BS"){
-      variance_method <- bootstrap_variance
-    } 
-    
-    for(l in 1:nstudies){
-      data_lth_study <- cbind(X[[l]], t[[l]], y[[l]]); colnames(data_lth_study) <- c(new_cov_names, "treatment", "y")
-      x_train <- model.matrix(model_formula, data = data_lth_study)[, -1]
-      
-      # fit cv.glmnet su dataset originale
-      cv.obj <- cv.glmnet(x_train, y = y[[l]], nfolds = 10, alpha = 0,
-                            penalty.factor = penalty_factors, lambda = lambda_seq)
-      
-      # predictions per ITE
-      predictions[, l] <- as.numeric(predict(cv.obj, newx = x1_test, s = "lambda.min") -
-                               predict(cv.obj, newx = x0_test, s = "lambda.min"))
-
-      variance[, l] <- variance_method(
-      data_train = data_lth_study,
+    variance_method <- get_variance_wrapper(
+      second_stage = second_stage,
       newX = newX,
       model_formula = model_formula,
-      x1_test = x1_test,
-      x0_test = x0_test,
       penalty_factors = penalty_factors,
-      lambda_seq = lambda_seq
-  )
+      lambda_seq = lambda_seq,
+      N_boot = N_sample_bootstrap
+    )
+
+    for(l in 1:nstudies){
+     # fit cv.glmnet su dataset originale
+     data[[l]]$y <- data[[l]][[outcome]]
+      cv.obj <- ridge_fit_model(data = data[[l]], model_formula = model_formula, 
+                                lambda_seq = lambda_seq, penalty_factors = penalty_factors,
+                                cov_names = covariate_names)
+      
+      # predictions per ITE
+      predictions[, l] <- ridge_predict_ite(model = cv.obj, x1_test = x1_test, x0_test = x0_test) 
+
+      variance[, l] <- variance_method(data[[l]])
 
       
     }
@@ -98,3 +94,57 @@ Ridge_fit <- function(y, X, t, newX, second_stage, N_sample_bootstrap = 100){
 
     return(pooled_predictions)
 }
+
+ridge_fit_model <- function(data, model_formula, lambda_seq, 
+                            penalty_factors){
+
+      x_train <- ridge_build_design_matrix(X = data, mod.formula = model_formula)
+      
+      # fit cv.glmnet su dataset originale
+      cv.obj <- cv.glmnet(x_train, y = data$y, nfolds = 10, alpha = 0,
+                            penalty.factor = penalty_factors, lambda = lambda_seq)
+
+      return(cv.obj)
+    
+}
+
+
+get_variance_wrapper <- function(second_stage, 
+                                 newX,
+                                 model_formula,
+                                 penalty_factors = NULL,
+                                 lambda_seq = NULL,
+                                 N_boot = 100){
+
+      second_stage <- toupper(second_stage)
+      
+      if(second_stage == "OLS+IV"){
+        # ritorna una funzione che prende solo data_train
+        return(function(data_train){
+          get_linear_model_variance(
+            data_train = data_train,
+            newX = newX,
+            model_formula = model_formula
+          )
+        })
+        
+      } else if(second_stage == "BS"){
+        # ritorna una funzione che prende solo data_train
+        return(function(data_train){
+          ridge_bootstrap_variance(
+            data_train = data_train,
+            newX = newX,
+            model_formula = model_formula,
+            penalty_factors = penalty_factors,
+            lambda_seq = lambda_seq,
+            N_boot = N_boot
+          )
+        })
+        
+      } else {
+        stop("second_stage must be either 'OLS+IV' or 'BS'")
+      }
+    } 
+
+
+
