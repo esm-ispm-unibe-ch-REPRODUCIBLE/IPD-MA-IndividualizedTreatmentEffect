5e9336741b93e89d08e00735f34b30c1531c7142
m-maina12 <150496376+m-maina12@users.noreply.github.com>
2026-03-11 10:19:43 +0100

    update

--- LinearShrinkage/LASSO_fitmodel.R
+++ LinearShrinkage/LASSO_fitmodel.R
@@ -2,31 +2,30 @@
 library(glmnet)
 library(matrixStats)
 
-lasso_predict_ite <- function(model, newX){
-  x1_test <- cbind(1, newX, newX) 
-  x0_test <- cbind(0, newX, 0 * newX) 
-
-  ite <- as.numeric(predict(cv.obj, newx = x1_test, s = "lambda.min") -
-                               predict(cv.obj, newx = x0_test, s = "lambda.min"))
+lasso_predict_ite <- function(model, # Model object
+                              x1_test,
+                              x0_test){
+  ite <- as.numeric(predict(model, newx = x1_test, s = "lambda.min") -
+                      predict(model, newx = x0_test, s = "lambda.min"))
 
   return(ite)
 }
 
-lasso_bootstrap_variance <- function(data_train, newX, model_formula,
-                               x1_test, x0_test,
-                               penalty_factors, lambda_seq,
-                               N_boot = 100){
+lasso_bootstrap_variance <- function(data_train, # Original training data
+                                     newX, # New covariates
+                                     model_formula, # Model formula
+                                     penalty_factors, # Penalty factors for the regression 
+                                     lambda_seq, # Grid of lambda parameters
+                                     N_boot = 100 # Numbers of bootstrap iteration
+                                     ){
 
   bootstrap_preds <- matrix(nrow = nrow(newX), ncol = N_boot)
 
   for(m in 1:N_boot){
-
       bs_data <- get_bootstrap_sample(X = data_train)
-
-      x_train_bs <- model.matrix(model_formula, data = bs_data)[, -1]
-
+      x_train_bs <- lasso_build_design_matrix(X = bs_data, mod.formula = model_formula)
       cv_bs <- cv.glmnet(
-          x_train_bs,
+          x = x_train_bs,
           y = bs_data$y,
           nfolds = 5,
           alpha = 1,
@@ -34,10 +33,7 @@ lasso_bootstrap_variance <- function(data_train, newX, model_formula,
           lambda = lambda_seq
       )
 
-      bootstrap_preds[, m] <- as.numeric(
-          predict(cv_bs, newx = x1_test, s = "lambda.min") -
-          predict(cv_bs, newx = x0_test, s = "lambda.min")
-      )
+      bootstrap_preds[, m] <- lasso_predict_ite(model = cv_bs, newX = newX)
   }
 
   matrixStats::rowVars(bootstrap_preds)
@@ -47,49 +43,48 @@ lasso_build_design_matrix <- function(X, mod.formula){
   return(model.matrix(mod.formula, data = X)[, -1])
 }
 
-lasso_ipd_ite <- function(data, newX, covariate_names, second_stage, N_sample_bootstrap = 100,
-  covariate_names){
-    # y = list of outcomes
-    # X = list of datasets with covariates
-    # t = list of treatment
-
-    n_covariates <- ncol(data) - 2
+lasso_ipd_ite <- function(data, 
+                          newX, 
+                          covariate_names, 
+                          outcome = "response",
+                          treatment = "trt",  
+                          second_stage, 
+                          N_sample_bootstrap = 100
+                          ){
+    n_covariates <- length(covariate_names)
     nstudies <- length(data)
 
     lambda_seq <- 10 ^ seq(2, -3, by = -0.3)
     penalty_factors <- c(rep(0, 1 + n_covariates), rep(1, n_covariates))
 
-    formula_str <- paste("y ~ treatment * (", paste(covariate_names, collapse = " + "), ")", 
+    formula_str <- paste("y ~ ", treatment, " * (", paste(covariate_names, collapse = " + "), ")", 
                          sep = "")
     model_formula <- as.formula(formula_str)
 
     predictions <- variance <- matrix(nrow = nrow(newX), ncol = nstudies)
+    x1_test <- model.matrix(model_formula, data = cbind(newX, treatment = 1))[,-1]
+    x0_test <- model.matrix(model_formula, data = cbind(newX, treatment = 0))[,-1] 
     
-    second_stage <- toupper(second_stage)
-    if(second_stage == "OLS+IV"){
-      variance_method <- get_linear_model_variance
-    } else (second_stage == "BS"){
-      variance_method <- bootstrap_variance
-    } 
+    variance_method <- get_variance_wrapper(
+      second_stage = second_stage,
+      newX = newX,
+      model_formula = model_formula,
+      penalty_factors = penalty_factors,
+      lambda_seq = lambda_seq,
+      N_boot = N_sample_bootstrap
+    )
 
     for(l in 1:nstudies){
      # fit cv.glmnet su dataset originale
+     data[[l]]$y <- data[[l]][[outcome]]
       cv.obj <- LASSO_fit_model(data = data[[l]], model_formula = model_formula, 
                                 lambda_seq = lambda_seq, penalty_factors = penalty_factors,
                                 cov_names = covariate_names)
       
       # predictions per ITE
-      predictions[, l] <- lasso_predict_ite(model = cv.obj, newX = newX)
+      predictions[, l] <- lasso_predict_ite(model = cv.obj, x1_test = x1_test, x0_test = x0_test) 
 
-      variance[, l] <- variance_method(
-      data_train = data_lth_study,
-      newX = newX,
-      model_formula = model_formula,
-      x1_test = x1_test,
-      x0_test = x0_test,
-      penalty_factors = penalty_factors,
-      lambda_seq = lambda_seq
-  )
+      variance[, l] <- variance_method(data[[l]])
 
       
     }
@@ -101,14 +96,76 @@ lasso_ipd_ite <- function(data, newX, covariate_names, second_stage, N_sample_bo
 }
 
 LASSO_fit_model <- function(data, model_formula, lambda_seq, 
-                            penalty_factors, cov_names){
+                            penalty_factors){
 
-      x_train <- lasso_build_design_matrix(X = data[, cov_names], mod.formula = model_formula)
+      x_train <- lasso_build_design_matrix(X = data, mod.formula = model_formula)
       
       # fit cv.glmnet su dataset originale
-      cv.obj <- cv.glmnet(x_train, y = y[[l]], nfolds = 10, alpha = 1,
+      cv.obj <- cv.glmnet(x_train, y = data$y, nfolds = 10, alpha = 1,
                             penalty.factor = penalty_factors, lambda = lambda_seq)
 
       return(cv.obj)
     
 }
+
+
+get_variance_wrapper <- function(second_stage, 
+                                 newX,
+                                 model_formula,
+                                 penalty_factors = NULL,
+                                 lambda_seq = NULL,
+                                 N_boot = 100){
+
+      second_stage <- toupper(second_stage)
+      
+      if(second_stage == "OLS+IV"){
+        # ritorna una funzione che prende solo data_train
+        return(function(data_train){
+          get_linear_model_variance(
+            data_train = data_train,
+            newX = newX,
+            model_formula = model_formula
+          )
+        })
+        
+      } else if(second_stage == "BS"){
+        # ritorna una funzione che prende solo data_train
+        return(function(data_train){
+          lasso_bootstrap_variance(
+            data_train = data_train,
+            newX = newX,
+            model_formula = model_formula,
+            penalty_factors = penalty_factors,
+            lambda_seq = lambda_seq,
+            N_boot = N_boot
+          )
+        })
+        
+      } else {
+        stop("second_stage must be either 'OLS+IV' or 'BS'")
+      }
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- UtilityScript_general.R
+++ UtilityScript_general.R
@@ -1,32 +1,7 @@
 # Utility script
-# Function for model performances
-assess_performances <- function(ITE_actual, ITE_predicted){
-  ok <- complete.cases(ITE_actual, ITE_predicted)
-  ITE_actual <- ITE_actual[ok]
-  ITE_predicted <- ITE_predicted[ok]
-  
-  # Median Absolute Error
-  MAE <- median(abs(ITE_actual - ITE_predicted))
-  
-  # Mean Bias
-  MB <- mean(ITE_actual - ITE_predicted)
-  
-  # R-squared
-  R2 <- 1 - mean((ITE_actual - ITE_predicted)^2)/var(ITE_actual)
-  
-  to_return <- c(MAE = MAE, 
-                 MSE = MSE, 
-                 MB = MB,
-                 R2 = R2)
-  
-  return(t(to_return))
-}
 
-# Pooling methods
+# Bootstrap
 
-# 2. Bootstrap
-
-# We said 100 bootstrap sample
 
 get_bootstrap_sample <- function(X){
   n <- nrow(X)
@@ -34,32 +9,22 @@ get_bootstrap_sample <- function(X){
   return(X[index, ])
 }
 
-ols_iv_variance <- function(data_train, newX, model_formula){
-  get_linear_model_variance(
-      data_train = data_train,
-      new_patients_data = newX,
-      formula = model_formula
-  )
-}
-
-# 3. Linear model variance
 get_linear_model_variance <- function(data_train,
-                                      new_patients_data,
-                                      formula) {
+                                      newX,
+                                      model_formula) {
   
   # Model fit
-  ausiliar_model <- lm(formula, data = data_train)
+  ausiliar_model <- lm(model_formula, data = data_train)
   
   # Cov matrixes
   Vbeta <- vcov(ausiliar_model)
   
   # New patients data; under treatment and control
-  data_1 <- new_patients_data %>% mutate(treatment = 1)
-  data_0 <- new_patients_data %>% mutate(treatment = 0)
+  data_1 <- cbind(newX, treatment = 1)
+  data_0 <- cbind(newX, treatment = 0)
   
-  formula_no_y <- delete.response(terms(formula))
-  x1_test <- model.matrix(formula_no_y, data = data_1)
-  x0_test <- model.matrix(formula_no_y, data = data_0)
+  x1_test <- model.matrix(model_formula, data = data_1)[, -1]
+  x0_test <- model.matrix(model_formula, data = data_0)[, -1]
   
   diff_x <- x1_test - x0_test
   
@@ -68,29 +33,3 @@ get_linear_model_variance <- function(data_train,
   
   return(as.numeric(variances))
 }
-
-# LINEAR MODEL VARIANCES
-
-# Names of the covariates and create the formula
-
-x_vars <- grep("^x", names(data_training[[1]][[1]][[1]]), value = TRUE)
-formula_str <- paste("y ~ treatment * (", paste(x_vars, collapse = " + "), ")", 
-                     sep = "")
-model_formula <- as.formula(formula_str)
-
-linear_model_variances <- vector("list", length = nscenarios)
-for(i in 1:nscenarios){
-  temp_matrix <- vector("list", length = nsim)
-  for(j in 1:nsim){
-    temp_simulation <- matrix(nrow = nrow(data_testing[[i]][[j]]),
-                              ncol = nstudies)
-    for(l in 1:nstudies){
-      temp_simulation[, l] <- get_linear_model_variance(data_train = data_training[[i]][[j]][[l]], 
-                                                        new_patients_data = data_testing[[i]][[j]],
-                                                        formula = model_formula)
-    }
-    temp_matrix[[j]] <- temp_simulation
-  }
-  linear_model_variances[[i]] <- temp_matrix
-}
-
